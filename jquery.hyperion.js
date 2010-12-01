b6ad3c2a4e46d71142ff35528132709de6970fd2
wusher <willusher+github@gmail.com>
2010-11-30 20:55:59 -0800

    moved pulgin to seperate file

---
+++ jquery.hyperion.js
@@ -0,0 +1,46 @@
+(function($){
+  $.fn.make_magic = function(options){
+    var defaults = { 
+      singleItems : [],
+      multipleItems : [],
+      pollTimeout : 3000
+    };
+    var options = $.extend(defaults,options);
+
+    return this.each(function () {
+      //build functions 
+      var onValuesChanged = function () {
+        var params = new Array();
+        for (var id in options.singleItems){
+          params[options.singleItems[id]] = parseFloat($("#" + options.singleItems[id]).val());
+        }
+        for (var klass in options.multipleItems){
+          var collection = new Array();
+          var items = $("." + options.multipleItems[klass]);
+          $.each(items, function(index,value){
+            collection[index] = parseFloat($(value).val());
+          });
+          params[options.multipleItems[klass]] = collection;
+        }
+        calculate_total(params);
+      };
+      var pollForChanges = function () {
+        onValuesChanged();
+        setTimeout(pollForChanges, options.pollTimeout);
+      };
+      // set up on change events 
+      for (var id in options.singleItems){
+        $("#" + options.singleItems[id]).change(onValuesChanged());
+      }
+      for (var klas in options.multipleItems){
+        $("." + options.multipleItems[klas]).change(onValuesChanged());
+      }
+      //at the end call the function
+      pollForChanges();
+         
+
+    });
+  };
+})(jQuery);
+
+
