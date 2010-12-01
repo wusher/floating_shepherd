8303a00aaaab77fca4cef44ada34354dad166ef6
wusher <willusher+github@gmail.com>
2010-12-01 06:58:26 -0800

    added new defaults

--- jquery.hyperion.js
+++ jquery.hyperion.js
@@ -3,22 +3,35 @@
     var defaults = { 
       singleItems : [],
       multipleItems : [],
-      pollTimeout : 3000
+      pollTimeout : 3000, 
+      convertEmptyToZero : true,
+      convertInvalidToZero : true
     };
     var options = $.extend(defaults,options);
 
     return this.each(function () {
+      var convertValueToFloat = function(value){
+        var result = parseFloat(value);
+        if (isNaN(result)){
+          if (options.convertValueToFloat && (value || value.length <= 0)){
+            result = 0
+          } else if (options.convertInvalidToZero) {
+            result = 0
+          }
+        }
+        return result; 
+      };
       //build functions 
       var onValuesChanged = function () {
         var params = new Array();
         for (var id in options.singleItems){
-          params[options.singleItems[id]] = parseFloat($("#" + options.singleItems[id]).val());
+          params[options.singleItems[id]] = convertValueToFloat($("#" + options.singleItems[id]).val());
         }
         for (var klass in options.multipleItems){
           var collection = new Array();
           var items = $("." + options.multipleItems[klass]);
           $.each(items, function(index,value){
-            collection[index] = parseFloat($(value).val());
+            collection[index] = convertValueToFloat($(value).val());
           });
           params[options.multipleItems[klass]] = collection;
         }
